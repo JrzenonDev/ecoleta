f79e4021b66bbc298850546856663fddc4e65de6
JrzenonDev <jrobertoonb@gmail.com>
2020-06-03 00:00:56 -0300

    chore: separando as rotas em módulo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
-  response.json([
-    'Jose Roberto',
-    'Norbet',
-    'Cleston',
-    'Boston'
-  ]);
-});
+app.use(routes);
+
 
 app.listen(3333);
