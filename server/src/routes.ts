18eca3313b3fa35e3fa9eddb166fed34980fa9f1
JrzenonDev <jrobertoonb@gmail.com>
2020-06-07 14:03:14 -0300

    chore: comentário sinalizando que o Router é utilizado neste caso para desacoplar as rotas do arquivo principal

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+// serve para desacoplar as rotas do arquivo principal
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
+  return response.json({message: 'Hello World!'});
 });
 
 export default routes;
