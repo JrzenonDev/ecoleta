7d33c8a0ca4aebc4e1de77272786f7f1735623df
JrzenonDev <jrobertoonb@gmail.com>
2020-06-10 01:33:34 -0300

    chore: correção no nome da coluna na tabela de items

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -11,7 +11,7 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('items', table => {
     table.increments('id').primary();
     table.string('image').notNullable();
-    table.string('titulo');
+    table.string('title');
   })
 }
 
