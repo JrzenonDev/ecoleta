a1ceb67cdd00772e638a9bb951b59b9aca81cdd5
JrzenonDev <jrobertoonb@gmail.com>
2020-06-10 01:43:30 -0300

    chore: definindo rota para arquivos estáticos da aplicação utilizando o: express.static, guardando arquivos svg dos items de coleta

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { json } from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(json());
 app.use(routes);
+
+// utilizado para servir arquivos estáticos na aplicação (função do exprees)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 
 app.listen(3333);
