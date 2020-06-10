38d736b061d9bf89d80d7f780a2c87d2307d2931
JrzenonDev <jrobertoonb@gmail.com>
2020-06-10 02:05:56 -0300

    chore: serealizando a url de image items, rota get items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,8 +9,16 @@ routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
   // como demora um pouco a busca no bd, utilizamos o await
 
+  // serealizando as informações de ulr de imagem para fornecer a nossa aplicação
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
 
-  return response.json({items});
+
+  return response.json({serializedItems});
 });
 
 export default routes;
