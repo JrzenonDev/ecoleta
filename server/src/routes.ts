8647286c19b0c416a425c543646886a21902f1b5
JrzenonDev <jrobertoonb@gmail.com>
2020-06-11 23:55:05 -0300

    refact:  desacoplando as funções das rotas para controllers, montando um pathener para melhor organização

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController';
+
 // (Router) = serve para desacoplar as rotas do arquivo principal
 const routes = express.Router();
+
+// instancia das classes
+const pointsController = new PointsController();
 
 routes.get('/items', async (request, response) => {
 
@@ -23,50 +28,6 @@ routes.get('/items', async (request, response) => {
 });
 
 
-routes.post('/points', async (request, response) => {
-
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body;
-
-  // transaction = caso uma das querys falhe a outra também não e executada
-  const trx = await knex.transaction();
-
-  // após fazer o insert o knex retorna os ids, que neste caso é somente um id
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  });
-
-  const point_id = insertedIds[0];
-
-  // aqui pegamos o id do points que foi inserido acima, para passar no segund insert que é a chace estrangeira
-  // o item_id do map é cada id dos items que foi recebido no request.body (lampada, óleo, etc)
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id
-    };
-  })
-
-
-  await trx('point_items').insert(pointItems);
-
-  return response.json({ success: true });
-
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
