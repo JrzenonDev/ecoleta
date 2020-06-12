e35d7bbefe41f9121087167e5e0e12ea8eb02154
JrzenonDev <jrobertoonb@gmail.com>
2020-06-11 23:24:38 -0300

    chore: rota de cadstro de ponto de coleta, com função já cadastrando no banco de dados, tanto na tabela de point como em point_items (chave estrangeira)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,6 +12,7 @@ routes.get('/items', async (request, response) => {
   // serealizando as informações de ulr de imagem para fornecer a nossa aplicação
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
@@ -19,6 +20,53 @@ routes.get('/items', async (request, response) => {
 
 
   return response.json({serializedItems});
+});
+
+
+routes.post('/points', async (request, response) => {
+
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  // transaction = caso uma das querys falhe a outra também não e executada
+  const trx = await knex.transaction();
+
+  // após fazer o insert o knex retorna os ids, que neste caso é somente um id
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  // aqui pegamos o id do points que foi inserido acima, para passar no segund insert que é a chace estrangeira
+  // o item_id do map é cada id dos items que foi recebido no request.body (lampada, óleo, etc)
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    };
+  })
+
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true });
+
 });
 
 export default routes;
